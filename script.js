623ed9b90b51e01467c066b6dcc3d88a1222276d
Chris Grovers <chrisgrovers@gmail.com>
2015-05-26 09:40:46 -0700

    (feat) compare a second team

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // gosugamers has info on past matches
 // csgolounge has percentage wins
     // data on who won
+
+
+// pull data from each individual player on each team?
+    // k/d ratio next to player names
+    // favorite map, etc, etc.
+
 var betHelper = angular.module('betHelper', [])
 
 betHelper.controller('MainController', function($scope, csgolounge) {
@@ -9,7 +15,56 @@ betHelper.controller('MainController', function($scope, csgolounge) {
     $scope.teamName = "TEAM NAME HERE";
     $scope.compareName = "Compare A Team";
     $scope.matches = [];
-    $scope.compareMatches = [];
+    $scope.secondMatches = [];
+
+    $scope.compareMatches = function() {
+        // need to add class '.compare' to 
+
+        console.log('hello');
+        $scope.secondMatches=[];
+        csgolounge.getStats()
+            .then(function(data) {
+            // resp.data should be a big list of all matches in csgolounge
+            // need to filter this data by input name
+            // input should match either "team_a" or "team_b"
+                // for each object that matches, push to object array
+                // really bad. Do I really want to iterate over 3000 objects?
+                for (var i = 0; i < data.matches.length; i++) {
+                    var currentMatch = data.matches[i]
+                    if (currentMatch.team_a === $scope.compareName) {
+                        console.log('a match was found!');
+                        var match = {
+                            vs: currentMatch.team_b,
+                            odds: currentMatch.team_a_odd,
+                            date: currentMatch.match_date,
+                            link: currentMatch.match_link,
+                            type: currentMatch.match_type,
+                            won: (currentMatch.winner === $scope.compareName)
+                        }
+                        $scope.secondMatches.push(match);
+
+                    } else if (currentMatch.team_b === $scope.compareName) {
+                        console.log('a match was found!');
+                        var match = {
+                            vs: currentMatch.team_a,
+                            odds: currentMatch.team_b_odd,
+                            date: currentMatch.match_date,
+                            link: currentMatch.match_link,
+                            type: currentMatch.match_type,
+                            won: (currentMatch.winner === $scope.compareName)
+                        }
+                        if (match) {
+                            $scope.secondMatches.push(match);
+                        } else {
+                            alert('Team not found!');
+                        }
+                    }
+                }
+            })
+            .catch(function(err) {
+                console.error(err);
+            })
+    }
 
     $scope.getBets = function() {
         console.log('hello');
@@ -20,12 +75,10 @@ betHelper.controller('MainController', function($scope, csgolounge) {
             // need to filter this data by input name
             // input should match either "team_a" or "team_b"
                 // for each object that matches, push to object array
-                console.log(this.text);
-                var teamName = $scope.teamName;
                 // really bad. Do I really want to iterate over 3000 objects?
                 for (var i = 0; i < data.matches.length; i++) {
                     var currentMatch = data.matches[i]
-                    if (currentMatch.team_a.ignoreCase === teamName.ignoreCase) {
+                    if (currentMatch.team_a === $scope.teamName) {
                         console.log('a match was found!');
                         var match = {
                             vs: currentMatch.team_b,
@@ -33,11 +86,11 @@ betHelper.controller('MainController', function($scope, csgolounge) {
                             date: currentMatch.match_date,
                             link: currentMatch.match_link,
                             type: currentMatch.match_type,
-                            won: (currentMatch.winner === teamName)
+                            won: (currentMatch.winner === $scope.teamName)
                         }
                         $scope.matches.push(match);
 
-                    } else if (currentMatch.team_b.ignoreCase === teamName.ignoreCase) {
+                    } else if (currentMatch.team_b === $scope.teamName) {
                         console.log('a match was found!');
                         var match = {
                             vs: currentMatch.team_a,
@@ -45,13 +98,11 @@ betHelper.controller('MainController', function($scope, csgolounge) {
                             date: currentMatch.match_date,
                             link: currentMatch.match_link,
                             type: currentMatch.match_type,
-                            won: (currentMatch.winner === teamName)
+                            won: (currentMatch.winner === $scope.teamName)
                         }
                         $scope.matches.push(match);
                     }
                 }
-                console.log('matches data');
-                console.dir($scope.matches);
             })
             .catch(function(err) {
                 console.error(err);
